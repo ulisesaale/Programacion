08ec444107846d68cd8dce9f2bfa6b52195f7eb7
ulisesaale <ulisesaale@gmail.com>
2023-08-22 20:23:03 -0300

    Ejercicio Nº2

--- Trabajos Practicos/Practico 3/Test Ejercicio 2/sketch.ino
+++ Trabajos Practicos/Practico 3/Test Ejercicio 2/sketch.ino
@@ -1,18 +1,17 @@
 //Practico 3 - Ejercicio 2 - ESP32
 //Encender un LED con un pulsador y cuando se suelta se apaga el LED
-//Configuracion en Wokwi
 
 void setup() {
-pinMode(4, OUPUT);   //Se asigna la entrada Led
-pinMode(5, INPUT);    //Se asigna la entrada Pulsador
+  pinMode(4, OUTPUT);   //Se asigna la entrada Led
+  pinMode(5, INPUT);    //Se asigna la entrada Pulsador
 }
 
 void loop() {
    if  (digitalRead (5) == HIGH) {  //Si el Pulsador esta apretado
        digitalWrite(4, HIGH);       //Encender el LED
-}
-else  
+   }
+  else  
       {digitalWrite(4, LOW);        //Sino esta apretado el pulsador, apagar el LED
-}
+  }
 
 }
